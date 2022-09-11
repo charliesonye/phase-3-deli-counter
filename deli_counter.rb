7977d406a94b511c37bb2aaa2d5cd2a5e49cfda8
Charles <c_onyemem@live.com>
2022-09-10 21:38:45 -0700

    commit tree dda9caf8b099504e5f016892170fd7e718a23382

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+require 'pry'
+
+def line(line)
+    customer_line = "The line is currently: "
+   if line == []
+    puts "The line is currently empty."
+   else
+    i = 0
+    while i < line.length
+        customer_line.concat "#{i + 1}. #{line[i]} "
+        i += 1
+    end
+     puts customer_line.chop
+
+   end
+
+    #Shows everyone their current place in line
+    #Need to print each element within an array
+    #Conditional to state when array is empty, a dedicated
+    #string will show
+end
+
+
+def take_a_number(line, new_customer)
+    if line == []
+        line.push new_customer
+        puts "Welcome, #{new_customer}. You are number #{line.index(new_customer) + 1} in line."
+    end
+end
+
+
